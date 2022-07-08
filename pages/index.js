d857b11d49e9cbea2b853947a3aeaaff89508863
“emilykiss” <emilylanekiss@gmail.com>
2022-07-08 15:35:04 -0700

    fixed deployment

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Welcome to Emily's Portfolio!</h1>
+        <h1>Welcome to my Portfolio!</h1>
       </main>
     </div>
   )
