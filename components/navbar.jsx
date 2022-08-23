0027635ef00daca94a9e18a8afa5df974ac941e7
“emilykiss” <emilylanekiss@gmail.com>
2022-08-22 17:05:35 -0700

    changed a to link

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,45 +2,40 @@ import Link from "next/link"
 export default function Navbar() {
   return (
     <>
-      <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-400 mb-3">
-        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-            <a
-              class="text-md font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="/contact"
-            >
-              EK
-            </a>
-           
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-400 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+            <Link href="/contact">
+              <span className="px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75">
+                EK
+              </span>
+            </Link>
           </div>
           <div
-            class="lg:flex flex-grow items-center"
+            className="lg:flex flex-grow items-center"
             id="example-navbar-warning"
           >
-            <ul class="flex flex-col lg:flex-row list-none ml-auto">
-              <li class="nav-item">
-                <a
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="/"
-                >
-                  About
-                </a>
+            <ul className="flex flex-col lg:flex-row list-none ml-auto">
+              <li className="nav-item">
+                <Link href="/">
+                  <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
+                    About
+                  </span>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="/work"
-                >
-                  Work
-                </a>
+              <li className="nav-item">
+                <Link href="/work">
+                  <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
+                    Work
+                  </span>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="/contact"
-                >
-                  Contact
-                </a>
+              <li className="nav-item">
+                <Link href="/contact">
+                  <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
+                    Contact
+                  </span>
+                </Link>
               </li>
             </ul>
           </div>
