a83d3cc5b362c3ede415221cd5f23c9341b146dd
“emilykiss” <emilylanekiss@gmail.com>
2023-03-14 12:29:29 -0700

    split screen

--- pages/work.jsx
+++ pages/work.jsx
@@ -5,16 +5,15 @@ import styles from "../styles/Home.module.css"
 
 export default function Work() {
   return (
-    <main>
-  
+    <main className="bg-blue-200">
       <div className="grid grid-cols-2 gap-1">
-        <div className=" bg-blue-100 rounded-xl m-10">
+        <div className=" bg-white rounded-xl m-10">
           <h2 className="flex justify-center text-center font-bold pt-12 text-3xl pl-4 text-blue-400">
             GooseCake Bakery
           </h2>
           <div className="m-8 flex justify-center">
             <Image
-              src="/s2.png"
+              src="/s22.jpg"
               className="rounded-3xl"
               alt="sb"
               width={300}
@@ -29,13 +28,13 @@ export default function Work() {
             </Link>
           </h6>
         </div>
-        <div className=" bg-blue-100 rounded-xl m-10">
+        <div className=" bg-white rounded-xl m-10">
           <h2 className="flex justify-center text-center font-bold pt-12 text-3xl text-blue-400">
             Pet Connection
           </h2>
           <div className="m-8 flex justify-center">
             <Image
-              src="/bruce.png"
+              src="/bru.jpg"
               className="rounded-3xl"
               alt="sb"
               width={300}
@@ -51,10 +50,9 @@ export default function Work() {
           </h6>
         </div>
       </div>
-      
 
       <div className="grid grid-cols-2 gap-1">
-        <div className="bg-blue-100 rounded-xl m-10">
+        <div className="bg-white rounded-xl m-10">
           <h2 className="flex justify-center mb-3 pt-8 font-bold text-3xl text-blue-400">
             CheckMate
           </h2>
@@ -68,18 +66,6 @@ export default function Work() {
             />
           </div>
 
-          {/* <h6 className="text-center tracking-widest m-6">
-            <a
-              href="https://www.figma.com/file/TBExpBCiLFOpg2Gn4taAV5/CheckMate?node-id=0%3A1&t=i5zkiQY0wIZ0aZ6J-1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              <span className="tracking-widest text-white bg-blue-400 text-xs hover:bg-blue-500 rounded-full py-2 px-4 ">
-                Figma
-              </span>
-            </a>
-          </h6> */}
           <h6 className="text-center m-6">
             <a
               href="https://62bf355e355a9e0c54111843--exquisite-liger-b9bd49.netlify.app/"
@@ -94,7 +80,7 @@ export default function Work() {
           </h6>
         </div>
 
-        <div className="rounded-xl bg-blue-100 m-10">
+        <div className="rounded-xl bg-white m-10">
           <h2 className="flex justify-center mb-3 pt-8 font-bold text-3xl text-blue-400">
             Ensemble
           </h2>
