680d068b6ce5b9b241153735f5d83e9ffa98f0f4
“emilykiss” <emilylanekiss@gmail.com>
2023-03-14 21:00:48 -0700

    font

--- pages/_document.js
+++ pages/_document.js
@@ -23,10 +23,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,500&family=Press+Start+2P&display=swap" rel="stylesheet"/>
-            
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,500&family=Press+Start+2P&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Bodoni+Moda:opsz,wght@6..96,500&family=Press+Start+2P&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
