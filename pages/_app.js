c8d6eef1f8c6fb6a531b25b6062ac96ae7fb2417
“emilykiss” <emilylanekiss@gmail.com>
2023-03-14 19:53:45 -0700

    font

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,16 @@ import "tailwindcss/tailwind.css"
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Bodoni+Moda:opsz,wght@6..96,500&family=Press+Start+2P&display=swap"
+        rel="stylesheet"
+      />
+
       <Navbar />
 
       <Component {...pageProps} />
-      
+
       {/* footer */}
     </>
   )
