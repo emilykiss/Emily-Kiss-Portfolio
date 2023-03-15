31bbd295587149cbf534a8c250db36958e89f31e
“emilykiss” <emilylanekiss@gmail.com>
2023-03-14 21:05:23 -0700

    lines removed

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -7,9 +7,7 @@ export default function Contact() {
       <h1 className="m-8 pt-16 text-center text-4xl text-blue-400">
         {`Wanna chat?`}
       </h1>
-      <h1 className="underline text-white text-8xl underline-offset-8 decoration-wavy decoration-sky-200">
-        hellohellohellohellohellohellohelloem
-      </h1>
+      
       <marquee>
         <div className="flex flex-row justify-around mt-24">
           <Image
@@ -61,10 +59,7 @@ export default function Contact() {
           </p>
         </div>
       </marquee>
-      <h1 className="underline text-white text-8xl underline-offset-8 decoration-wavy decoration-sky-200">
-        hellohellohellohellohellohellohelloem
-      </h1>
-
+     
       <h1 className="mt-8 text-center text-3xl font-semibold text-white"></h1>
     </div>
   )
