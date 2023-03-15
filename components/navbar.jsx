f01170e64b038fc5106740084ce51c01150f20bf
“emilykiss” <emilylanekiss@gmail.com>
2023-03-14 19:36:50 -0700

    split screen

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
       <nav className="w-full bg-blue-400 shadow">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex items-center justify-between py-4 md:py-8 md:block">
   
               <div className="md:hidden">
                 <button
